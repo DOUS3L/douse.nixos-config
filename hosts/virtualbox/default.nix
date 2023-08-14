e584dadf32e9f7cef7e0c2d001faf0c33ea7e925
DOUS3L <aldo101162@gmail.com>
2023-08-15 02:31:39 +0700

    add modules

--- hosts/virtualbox/default.nix
+++ hosts/virtualbox/default.nix
@@ -1,14 +1,14 @@
-{ configs, pkgs, user, ... }:
+{ pkgs, user, ... }:
 
 {
   imports = [
     ./hardware-configuration.nix
-    (fetchTarball {
-      url = "https://github.com/nix-community/nixos-vscode-server/tarball/master";
-      sha256 = "sha256:0sz8njfxn5bw89n6xhlzsbxkafb6qmnszj4qxy2w0hw2mgmjp829";
-    })
+    ../../modules/desktop/gnome/default.nix
+    ../../modules/nixos/services/vscode-server.nix
+
   ];
 
+  services.vscode-server.enable = true;
   users.users.${user} = {
     openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDOVbmxiD14/ZR1kePFEEhpGPjPZh1A4yQfa1cnqw8zBnpWa/EW2aNWqOMjGJ9m0C1GLYPRJZE3nDidSJ6fWjuuBF9fbnJRy+fnk8iJ52qrx0aB08BSBoTY+c8NsukxkNTbLXpHyEr6zvw+Gp2myH2yZ6rP0ZdYuIDvsIGJ9n8cE88/G64jwUgKfCkMuz7GAS/7ozaxNMHG6hm0cmWW4Uf6CCxTjDLH8SKpytN97Q8c9x6aZMRpdi2vv9ppiddNa00NZ66C6gG08crDX7w9wDlMUnt8OGf+rRNoIyluOzN24YyGQvpYwYs0qAtARH644eWRK+FfSOGWKBg/7ZHDBUz aldous@DESKTOP-N6T4RQQ" ];
   };
@@ -19,7 +19,7 @@
         enable = true;
         device = "/dev/sda";
         useOSProber = true;
-        configurationLimit = 15;
+        configurationLimit = 10;
       };
     };
   };
@@ -28,13 +28,6 @@
 
   services = {
     openssh.enable = true;
-    vscode-server.enable = true;
-    xserver = {
-      enable = true;
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
-      layout = "us";
-      xkbVariant = "";
-    };
   };
+
 }
