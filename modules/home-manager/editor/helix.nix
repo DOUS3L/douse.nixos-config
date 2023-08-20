3a6da1f6cf9f764251c971e6d548d27e3f0e6bab
DOUS3L <aldo101162@gmail.com>
2023-08-21 02:04:18 +0700

    update

--- modules/home-manager/editor/helix.nix
+++ modules/home-manager/editor/helix.nix
@@ -9,12 +9,12 @@
           line-number = "relative";
           lsp.display-messages = true;
         };
-        # keys.normal = {
-        #   space.space = "file_picker";
-        #   space.w = ":w";
-        #   space.q = ":q";
-        #   esc = [ "collapse_selection" "keep_primary_selection" ];
-        # };
+        keys.normal = {
+           space.space = "file_picker";
+           space.w = ":w";
+           space.q = ":q";
+           esc = [ "collapse_selection" "keep_primary_selection" ];
+         };
       };
     };
   };
