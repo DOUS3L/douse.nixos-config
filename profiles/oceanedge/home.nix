8e9e6f81d78baf5489805cf987275bb8445e9de4
DOUS3L <aldo1162@gmail.com>
2023-09-10 22:24:12 +0700

    update

--- profiles/oceanedge/home.nix
+++ profiles/oceanedge/home.nix
@@ -12,11 +12,11 @@
         sopsFile = ../../secrets/secrets_oceanedge.yaml;
       };
       gcpvpn0 = {
-        path = "${config.home.homeDirectory}/.secrets/alivpn0.ovpn";
+        path = "${config.home.homeDirectory}/.secrets/gcpvpn0.ovpn";
         sopsFile = ../../secrets/secrets_oceanedge.yaml;
       };
       gcpvpn1 = {
-        path = "${config.home.homeDirectory}/.secrets/alivpn0.ovpn";
+        path = "${config.home.homeDirectory}/.secrets/gcpvpn1.ovpn";
         sopsFile = ../../secrets/secrets_oceanedge.yaml;
       };
     };
